cdee7eab5f526eb2734bf9c62203e2aa9ec98dde
Shyam4801 <shyamnambiraja01@gmail.com>
2023-08-28 19:02:10 -0700

    removed old scripts

--- tests/mptest.py
+++
@@ -1,8 +0,0 @@
-from pp import YourClass
-
-if __name__ == '__main__':
-    your_instance = YourClass()
-        # your_instance.get_arrays()
-        # your_instance.parallel_processing()
-    res = your_instance.perf()
-    print(res)
